cc5a4576ed93f617c2274e3ddfd1d06e4468e45f
Cebucradugeorge <44407101+Cebucradugeorge@users.noreply.github.com>
2018-11-13 18:26:21 +0200

    Add files via upload

--- cam.js
+++ cam.js
@@ -1,8 +1,16 @@
-
 document.getElementById("id_logic_version").innerHTML ="Logic version: 2018.11.13.0";
-
+function on_ok(e)
+{
+document.getElementById("id_video").srcObject = stream;
+}
+function on_error(e)
+{
+//console.log("eroare camera");	
+alert("eroare camera");
+}
 function start ()
 {
-navigator.mediaDevices.getUserMedia().then().catch();
+	var p = {audio : true, video : true};
+navigator.mediaDevices.getUserMedia(p).then(on_ok).catch();
 
 }
