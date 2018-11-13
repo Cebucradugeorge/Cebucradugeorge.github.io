1928b810631a8e473b9bcd1224c14e8a856b54d3
Cebucradugeorge <44407101+Cebucradugeorge@users.noreply.github.com>
2018-11-13 18:40:22 +0200

    Add files via upload

--- cam.js
+++ cam.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML ="Logic version: 2018.11.13.0";
+document.getElementById("id_logic_version").innerHTML ="Logic version: 2018.11.13.1";
 function on_ok(e)
 {
 document.getElementById("id_video").srcObject = stream;
