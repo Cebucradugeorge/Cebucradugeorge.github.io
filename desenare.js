7e1d423467b03e09c96ecc226312aeb9c45c9bd6
Cebucradugeorge <44407101+Cebucradugeorge@users.noreply.github.com>
2018-11-20 18:46:25 +0200

    Add files via upload

--- desenare.js
+++ desenare.js
@@ -1,9 +1,9 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.11.20.2";
+document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.11.20.3";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch);
+canvas.addEventListener("touchmove", on_touch_move);
 var rect = canvas.getBoundingClientRect();
-
 function on_touch(e)
 {
 for (var i=0; i< e.changedTouches.length; i++)
@@ -11,11 +11,16 @@ for (var i=0; i< e.changedTouches.length; i++)
 	var context = canvas.getContext("2d");
 	context.beginPath();
 	context.arc(e.changedTouches.item(i).pageX - rect.left, e.changedTouches.item(i).pageY - rect.top,20,0,2 * Math.PI);
-	context.stroke();
-	
-	
+	context.stroke();	
+}	
 }
-	
-	
-	
+function on_touch_move(e)
+{
+for (var i=0; i< e.changedTouches.length; i++)
+{
+	var context = canvas.getContext("2d");
+	context.beginPath();
+	context.arc(e.changedTouches.item(i).pageX - rect.left, e.changedTouches.item(i).pageY - rect.top,20,0,2 * Math.PI);
+	context.stroke();	
+}	
 }
