c122f740349e26ecde0d5900b76b49c6137c556f
Cebucradugeorge <44407101+Cebucradugeorge@users.noreply.github.com>
2018-11-27 19:03:32 +0200

    Add files via upload

--- desenare.js
+++ desenare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.11.27.5";
+document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.11.27.6";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch);
