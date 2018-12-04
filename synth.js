b419ff00bd32d65a91bae346869a382e77ee940b
Cebucradugeorge <44407101+Cebucradugeorge@users.noreply.github.com>
2018-12-04 18:45:07 +0200

    Add files via upload

--- synth.js
+++ synth.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.5";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.6";
 var synth = window.speechSynthesis;
 function afiseaza_voci()
 {
@@ -16,6 +16,12 @@ function vorbeste()
 	enunt.lang = "em-US";
 	enunt.rate = 1.5;
 	enunt.volume = 0.7;
+	enunt.onend = function sfarsit_vorbeste;
+	doc.getElementById("id_button_vorbeste").disabled = true;
 	synth.speak(enunt);
 	
+}
+function sfarsit_vorbeste()
+{
+doc.getElementById("id_button_vorbeste").disabled = false;	
 }
