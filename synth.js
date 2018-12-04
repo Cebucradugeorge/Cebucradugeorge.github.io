e1eb1323e2e59761f996a01d10b9bf20bb03949e
Cebucradugeorge <44407101+Cebucradugeorge@users.noreply.github.com>
2018-12-04 18:16:02 +0200

    Add files via upload

--- synth.js
+++ synth.js
@@ -13,7 +13,7 @@ function vorbeste()
 {
 	var enunt = new SpeechSynthesisUtterance();
 	enunt.text = document.getElementById("id_text").value;
-	enunt.lang = "em-US";
+	enunt.lang = "de-DE";
 	synth.speak(enunt);
 	
 }
